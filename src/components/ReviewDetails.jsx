e777b27de5b660cc96f236b219af07ecae752fed
Betoml5 <betoml5@hotmail.com>
2022-10-02 11:54:27 -0500

    Add styles to review details note

--- src/components/ReviewDetails.jsx
+++ src/components/ReviewDetails.jsx
@@ -14,16 +14,23 @@ const ReviewDetails = () => {
       } = await findOneReviewAPI(id);
       setReview(attributes);
     } catch (error) {
-      console.log(error);
+      throw error;
     }
-    // setReview(data);
   };
 
   useEffect(() => {
     getReviewById();
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="divide-y-2 divide-dotted  divide-red-500 -rotate-3 cursor-pointer bg-[#ffc] max-w-md mx-4 md:mx-auto mt-20 rounded-sm shadow-xl hover:rotate-0 animate transition-all">
+      <h4 className="text-xl p-4 text-red-500 font-regular uppercase">
+        {review.title}
+      </h4>
+      <p className="p-4">{review.description}</p>
+      <p className="p-4 text-xs italic">{review.user}</p>
+    </div>
+  );
 };
 
 export default ReviewDetails;
