05a5158755cd0585c5de79b5eb9e80af2e3f56d1
Betoml5 <betoml5@hotmail.com>
2022-10-03 00:04:35 -0500

    Fix regex AGAIN

--- src/utils/Validations.js
+++ src/utils/Validations.js
@@ -1,5 +1,5 @@
-export function validateControlNumber(input) {
-  let regex = /(221|201|191|181|171)+[GC]+[0-9]+/i;
+function validateControlNumber(input) {
+  let regex = /(221|211|201|191|181|171)+[GC]+[0-9]+/i;
   return regex.test(input);
 }
 
