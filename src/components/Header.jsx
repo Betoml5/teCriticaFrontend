1475fb28811e76ada79e455e0363802da93075c5
Betoml5 <betoml5@hotmail.com>
2022-10-04 07:53:44 -0500

    Add spooky colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-[#00ADB5] flex justify-between items-center text-white  p-4">
+    <header className="bg-[#FF5B00] flex justify-between items-center text-white  p-4">
       <div className="flex items-center ">
-        <Link to="/" className="font-bold mr-2">
+        <Link to="/" className="font-bold mr-2 md:text-xl text-[#FFEE63]">
           TecNM Campus RC
         </Link>
         <Link
